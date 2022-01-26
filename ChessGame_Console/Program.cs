c24796fea93b7b994ec0bcc48ceaf62272132e05
Vinicius Oliveira <vinicius.marchesini@live.com>
2022-01-26 13:12:15 -0300

    Project Created

---
+++ ChessGame_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessGame_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
