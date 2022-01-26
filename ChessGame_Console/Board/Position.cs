1269c25a319ab348d84c4e32ac4e6e84460c2b95
Vinicius Oliveira <vinicius.marchesini@live.com>
2022-01-26 13:21:12 -0300

    Created Board Class

---
+++ ChessGame_Console/Board/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Board
+{
+    class Position
+    {
+        public int Line { get; set; }
+        public int Collum { get; set; }
+
+        public Position(int line, int collum)
+        {
+            Line = line;
+            Collum = collum;
+        }
+
+        public override string ToString()
+        {
+            return $"{Line}, {Collum}";
+        }
+    }
+}
--- ChessGame_Console/Program.cs
+++ ChessGame_Console/Program.cs
@@ -6,7 +6,7 @@ namespace ChessGame_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
