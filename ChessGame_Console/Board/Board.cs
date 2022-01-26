84e26510b38c9075e713c9cc908a4ab833033c37
Vinicius Oliveira <vinicius.marchesini@live.com>
2022-01-26 14:52:01 -0300

    Implementing BoardExceptions

--- ChessGame_Console/Board/Board.cs
+++ ChessGame_Console/Board/Board.cs
@@ -17,14 +17,46 @@ namespace Board
             pieces = new Piece[lines, collums];
         }
 
+        public bool PieceExist(Position pos)
+        {
+            ValidatePosition(pos);
+            return Piece(pos) != null;
+        }
+
         public void InputPiece(Piece piece, Position pos)
         {
+            if (PieceExist(pos))
+            {
+                throw new BoardException("Piece already exist in this position");
+            }
             pieces[pos.Line, pos.Collum] = piece;
             piece.Position = pos;
         }
         public Piece Piece(int line, int collum)
         {
             return pieces[line, collum];
+        }
+
+        public Piece Piece(Position pos)
+        {
+            return pieces[pos.Line, pos.Collum];
+        }
+
+        public bool ValidPosition(Position pos)
+        {
+            if (pos.Line < 0 || pos.Line > Lines || pos.Collum < 0 || pos.Collum > Collums)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!ValidPosition(pos))
+            {
+                throw new BoardException("Board position is not valid!");
+            }
         }
     }
 }
---
+++ ChessGame_Console/Board/BoardException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Board
+{
+    class BoardException : Exception
+    {
+        public BoardException(string msg) : base(msg)
+        {
+        }
+    }
+}
